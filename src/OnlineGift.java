e14f45ce3c67d73482a27d09b0db3444bc691957
krishnapradeep <krishnapradeep@krishna>
2018-01-21 18:41:39 +0530

    initial

---
+++ src/OnlineGift.java
@@ -0,0 +1,19 @@
+
+
+/**
+ *
+ * @author dks1
+ */
+public class OnlineGift {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login ll = new Login();
+        ll.setVisible(true);
+        // this.dispose();
+    }
+
+}
